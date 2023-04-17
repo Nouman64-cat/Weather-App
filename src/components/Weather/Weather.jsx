6bee70a00a2fab556b18384482e0f531101fa32e
Nouman Ejaz <112774247+Nouman64-cat@users.noreply.github.com>
2023-04-17 15:35:29 +0500

    Update Weather.jsx

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,29 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { AsyncPaginate } from 'react-select-async-paginate';
 import './weather.css'
 const Weather = () => {
-const [weatherData, setWeatherData] = useState(null);
-
+  const [weatherData, setWeatherData] = useState(null);
+  const [locationGeo, setLocationGeo] = useState(null);
   const [location, setLocation] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const WEATHER_API ="e1d2e14d3d96ad5395a7e94932a81580";
+  const GEOCODING_API = "59f531a7874a47aa854f6900e5f1bdfa";
+
+    axios
+    .get(`https://api.opencagedata.com/geocode/v1/json?q=${location}&key=${GEOCODING_API}`)
+    .then(response => {
+        const { lat, lng } = response.data.results[0].geometry;
+        setLatitude(lat);
+        setLongitude(lng);
+    })
+    .catch(error => {
+        console.log(error.message)
+    })
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        const { latitude, longitude } = position.coords;
-        setLocation({ latitude, longitude });
+        const { lat, lon } = position.coords;
+        setLocationGeo({ lat, lon });
       },
       (error) => {
         console.error(error);
       }
     );
   }, []);
-
   useEffect(() => {
-    if (location) {
-      const { latitude, longitude } = location;
+    if (locationGeo) {
+      const { lat, lon } = locationGeo;
       axios
         .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=75e7bb37bc2e886576ed06913667892b`
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=75e7bb37bc2e886576ed06913667892b`
         )
         .then((response) => {
           setWeatherData(response.data);
@@ -32,7 +47,20 @@ const [weatherData, setWeatherData] = useState(null);
           console.error(error);
         });
     }
-  }, [location]);
+  }, [locationGeo]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+    .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API}&units=metric`)
+    .then(response => {
+        console.log(response.data)
+        setWeatherData(response.data);
+    })
+    .catch(error => {
+        console.log(error.message)
+    })
+    };
 
   const getIcon = (weatherCode) => {
     switch (weatherCode) {
@@ -56,20 +84,33 @@ const [weatherData, setWeatherData] = useState(null);
         return '';
     }
   }
-  if(!weatherData) return (<div className='container'>Loading...</div>)
+  // if(!weatherData) return (<div className='container'>Loading...</div>)
   return (
     <div className='container'>
+
+      <form 
+        onSubmit={handleSubmit}
+        >
+          <input 
+            placeholder='Search for city'
+            type="text" 
+            value={location} 
+            onChange={event => setLocation(event.target.value)} 
+          />
+        <button type="submit">Get weather</button>
+      </form>
+
       {weatherData &&
-        <div className='weather-card'>
-          <div className='weather-text'>
-            <p className='country'>{weatherData.name} ,{weatherData.sys.country}</p>
-            <p className='description'>{weatherData.weather[0].description}</p>
-            <p className='temp'>{weatherData.main.temp}&deg;C</p>
+        <>
+          <div className='weather-card'>
+              <div className='weather-text'>
+                <p className='country'>{weatherData.name} ,{weatherData.sys.country}</p>
+                <p className='description'>{weatherData.weather[0].description}</p>
+                <p className='temp'>{weatherData.main.temp}&deg;C</p>
+              </div>
+              <img src={getIcon(weatherData.weather[0].icon)} alt={weatherData.weather[0].description} />
           </div>
-          <img src={getIcon(weatherData.weather[0].icon)} alt={weatherData.weather[0].description} />
-        </div>
-      }
-        <div className="weather-details">
+          <div className="weather-details">
             humidity: {weatherData && weatherData.main.humidity}%
             <br />
             wind: {weatherData && weatherData.wind.speed} m/s
@@ -77,7 +118,10 @@ const [weatherData, setWeatherData] = useState(null);
             pressure: {weatherData && weatherData.main.pressure} hPa
             <br />
             feels like: {weatherData && weatherData.main.feels_like} &deg;C
-        </div>
+          </div>
+       </>
+      }
+        
     </div>
   );
 }
